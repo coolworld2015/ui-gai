0dcf306435d94e0c9c1d54625310e6a2a7138905
coolworld2015 <wintermute2015@ukr.net>
2016-02-19 22:26:55 +0200

    [items] - added RegNum

--- app/src/app/app.js
+++ app/src/app/app.js
@@ -43,9 +43,9 @@
         $rootScope.numPerPageItems = 10;
 
         $rootScope.myConfig = {
-            webUrl: 'http://ui-gai.herokuapp.com/' //TODO Heroku MongoDB
+            //webUrl: 'http://ui-gai.herokuapp.com/' //TODO Heroku MongoDB
             //webUrl: 'http://localhost:3000/' //TODO Local MongoDB
-            //webUrl: 'http://localhost:3000/file/' //TODO Local JSON DB
+            webUrl: 'http://localhost:3000/file/' //TODO Local JSON DB
         };
 
         //$rootScope.mode = 'OFF-LINE (LocalStorage)'; //TODO !!! ONLY for Google Market
